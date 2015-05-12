69eb97afe8d2854bcfaa919044f163aede204b5a
mirjamsk <mirjamskarica@gmail.com>
2015-05-12 19:36:23 +0200

    Change schema

--- Deliveries/Resources/DDresources/DBschemas/Scripts/procedure_get_auction_rankings.sql
+++ Deliveries/Resources/DDresources/DBschemas/Scripts/procedure_get_auction_rankings.sql
@@ -1,7 +1,7 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_auction_rankings`(IN arg_auction_id INT)
 BEGIN
 SELECT w.*, 1 AS rank
-FROM currently_winning_bid as w
+FROM winning_bid as w
 WHERE w.bid_auction_id = arg_auction_id
 UNION
 
@@ -9,7 +9,7 @@ SELECT * FROM
  (SELECT  b.*, @rank:=@rank+1 AS rank
  FROM bid as b, (SELECT @rank := 1) AS r
  WHERE b.bid_auction_id = 1 and 
-	b.bid_id not in (SELECT w.bid_id FROM currently_winning_bid as w)  
+	b.bid_id not in (SELECT w.bid_id FROM winning_bid as w)  
     ORDER BY amount ASC) AS t;
     
 END
--- Deliveries/Resources/DDresources/DBschemas/Scripts/procedure_get_user_auction_ranking.sql
+++ Deliveries/Resources/DDresources/DBschemas/Scripts/procedure_get_user_auction_ranking.sql
@@ -2,7 +2,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `get_user_auction_ranking`(IN arg_us
 BEGIN
 
 SELECT 1 AS rank
-FROM currently_winning_bid as w
+FROM winning_bid as w
 WHERE w.bid_auction_id = arg_auction_id and w.bidder_id = arg_user_id
 
 UNION
@@ -11,7 +11,7 @@ SELECT rank FROM
  (SELECT  b.bidder_id, @rank:=@rank+1 AS rank
  FROM bid as b, (SELECT @rank := 1) AS r
  WHERE b.bid_auction_id =  arg_auction_id and 
-	b.bid_id not in (SELECT w.bid_id FROM currently_winning_bid as w)  
+	b.bid_id not in (SELECT w.bid_id FROM winning_bid as w)  
     ORDER BY amount ASC) AS br
 WHERE br.bidder_id = arg_user_id
 LIMIT 1;
