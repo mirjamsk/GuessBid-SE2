0ab176de1882355f6770b13143da124915ba1e04
mirjamsk <mirjamskarica@gmail.com>
2015-05-13 16:29:34 +0200

    Finish UX design

--- Deliveries/Resources/DDresources/DBschemas/Scripts/procedure_get_auction_rankings.sql
+++ Deliveries/Resources/DDresources/DBschemas/Scripts/procedure_get_auction_rankings.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `get_auction_rankings`(IN arg_auction_id INT)
+CREATE DEFINER=`guessbid`@`%` PROCEDURE `get_auction_rankings`(IN arg_auction_id INT)
 BEGIN
 SELECT w.*, 1 AS rank
 FROM winning_bid as w
@@ -8,8 +8,8 @@ UNION
 SELECT * FROM 
  (SELECT  b.*, @rank:=@rank+1 AS rank
  FROM bid as b, (SELECT @rank := 1) AS r
- WHERE b.bid_auction_id = 1 and 
+ WHERE b.bid_auction_id = arg_auction_id and 
 	b.bid_id not in (SELECT w.bid_id FROM winning_bid as w)  
     ORDER BY amount ASC) AS t;
-    
+
 END
